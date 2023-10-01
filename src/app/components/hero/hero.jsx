1553d8278017c5372067726ff75af4a94bc41f31
Andrei Savu <savuandreid@gmail.com>
2023-10-01 16:18:05 +0300

    useScroll animation for hero & intro

--- src/app/components/hero/hero.jsx
+++ src/app/components/hero/hero.jsx
@@ -1,12 +1,24 @@
+"use client";
+
 import styles from "./hero.module.scss";
 import Image from "next/image";
+import { useMotionValue, useScroll, useTransform } from "framer-motion";
+import { motion as m } from "framer-motion";
+import { useEffect } from "react";
 
 export default function Hero() {
+  const { scrollYProgress } = useScroll();
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.2]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
   return (
     <div className={styles.hero}>
-      <h1 className={styles.title}>Fashion • Jewellery</h1>
-
-      <Image className={styles.image} src="/hero.webp" alt="Fashion Jewellery" fill />
+      <m.h1 className={styles.title} style={{ opacity: opacity }}>
+        Fashion • Jewellery
+      </m.h1>
+      <m.div className={styles.imageWrap} style={{ scale: scale }}>
+        <Image className={styles.image} src="/hero.webp" alt="Fashion Jewellery" fill />
+      </m.div>
     </div>
   );
 }
--- src/app/components/intro/intro.jsx
+++ src/app/components/intro/intro.jsx
@@ -1,9 +1,20 @@
+"use client";
+
 import styles from "./intro.module.scss";
+import { useMotionValue, useScroll, useTransform } from "framer-motion";
+import { motion as m } from "framer-motion";
+import { useEffect } from "react";
 
 export default function Intro() {
+  const { scrollYProgress } = useScroll();
+
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+
   return (
     <div className={styles.intro}>
-      <h1 className={styles.title}>Fashion • Jewellery</h1>
+      <m.h2 className={styles.title} style={{ opacity: opacity }}>
+        Fashion • Jewellery
+      </m.h2>
     </div>
   );
 }
